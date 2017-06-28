54febc4bde2bcf018b1b71cb5ae6ebc2bb7dc6f8
Egarat Kaoroprat <egarat.kaoroprat@gmail.com>
2017-06-28 22:02:10 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './App.css';
+import SearchForm from './Components/SearchForm';
+import GifList from './Components/GifList';
+
+export default class App extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      gifs: []
+    };
+  }
+
+  render() { 
+    return (
+      <div>
+        <div className="main-header">
+          <div className="inner">
+            <h1 className="main-title">GifSearch</h1>
+            <SearchForm />      
+          </div>   
+        </div>    
+        <div className="main-content">
+          <GifList />
+        </div>
+      </div>
+    );
+  }
+}
