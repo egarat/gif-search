535c2d08a4cd234dac8ed413618fe221c5476e57
Egarat Kaoroprat <egarat.kaoroprat@gmail.com>
2017-06-28 23:03:17 +0200

    Add search feature

--- src/App.js
+++ src/App.js
@@ -15,23 +15,17 @@ export default class App extends Component {
   }
 
   componentDidMount() {
-    axios.get('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
+    this.performSearch();
+  }
+
+  performSearch = (query = 'funny') => {
+    axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=dc6zaTOxFJmzC`)
       .then(response => {
         this.setState({ gifs: response.data.data })
       })
       .catch(error => {
         console.log('Error fetching and parsing data', error);
       });
-
-    // Fetch API
-    // fetch('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
-    //   .then(response => response.json())
-    //   .then(responseData => {
-    //     this.setState({ gifs: responseData.data });
-    //   })
-    //   .catch(error => {
-    //     console.log('Error fetching and parsing data', error)
-    //   });
   }
 
   render() {
@@ -41,7 +35,7 @@ export default class App extends Component {
         <div className="main-header">
           <div className="inner">
             <h1 className="main-title">GifSearch</h1>
-            <SearchForm />      
+            <SearchForm onSearch={this.performSearch} />      
           </div>   
         </div>    
         <div className="main-content">
