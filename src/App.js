b6f331b62423a7e10c96c1925a0ebd6a2ebba375
Egarat Kaoroprat <egarat.kaoroprat@gmail.com>
2017-06-28 22:35:51 +0200

    Add ajax request to gyphy API with axios

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 import './App.css';
 import SearchForm from './Components/SearchForm';
 import GifList from './Components/GifList';
@@ -12,7 +14,28 @@ export default class App extends Component {
     };
   }
 
-  render() { 
+  componentDidMount() {
+    axios.get('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
+      .then(response => {
+        this.setState({ gifs: response.data.data })
+      })
+      .catch(error => {
+        console.log('Error fetching and parsing data', error);
+      });
+
+    // Fetch API
+    // fetch('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
+    //   .then(response => response.json())
+    //   .then(responseData => {
+    //     this.setState({ gifs: responseData.data });
+    //   })
+    //   .catch(error => {
+    //     console.log('Error fetching and parsing data', error)
+    //   });
+  }
+
+  render() {
+    console.log(this.state.gifs)
     return (
       <div>
         <div className="main-header">
